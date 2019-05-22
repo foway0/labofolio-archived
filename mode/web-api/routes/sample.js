f98a94c00a6348c07132abc9fe2aaf099e30cb95
foway <foway0@gmail.com>
2019-05-23 00:41:02 +0900

    add async wrap func & sequelize error handling

--- mode/web-api/routes/sample.js
+++ mode/web-api/routes/sample.js
@@ -2,14 +2,21 @@ const express = require('express');
 
 const {context} = require('../../../core');
 const code = context.getConst().statusCode;
+const utils = context.getUtils();
 
 const router = express.Router();
+
+const doAsync = fn => async (req, res, next) => await fn(req, res, next).catch(next);
 
 router.get('/', (req, res) => {
   res.status(code.OK).send(`This is my sample page!!!`);
 });
 
-router.get('/result', async (req, res) => {
+router.get('/error', doAsync(async (req, res) => {
+  throw new utils.error('woops', 'TEST', code.BAD_REQUEST);
+}));
+
+router.get('/list', doAsync(async (req, res) => {
   const sequelize = context.getMysql();
   const STATUS = context.getModels().sample.STATUS;
   const sample = context.getModels().sample(sequelize);
@@ -17,9 +24,12 @@ router.get('/result', async (req, res) => {
     where: {
       status: STATUS.valid,
     }
+  }).catch(err => {
+    // TODO send slack AND monitoring
+    throw new utils.error(err.name, err.original.code, code.BAD_REQUEST);
   });
 
   res.status(code.OK).json(result);
-});
+}));
 
 module.exports = router;
