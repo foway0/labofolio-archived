99164ea0ea6c731bd7cc42c2e5492c218ae02575
foway <foway0@gmail.com>
2019-07-28 23:00:05 +0900

    add UT

---
+++ tools/test/index.js
@@ -0,0 +1,21 @@
+const {before, beforeEach, after, afterEach} = require("mocha");
+
+before(function() {
+  // 블록 범위 내 모든 테스트 전에 실행
+  console.log('before');
+});
+
+beforeEach(function() {
+  // 블록 범위 내 각 테스트 직전에 실행
+  console.log('before each');
+});
+
+afterEach(function() {
+  // 블록 범위 내 각 테스트 직후에 실행
+  console.log('after each');
+});
+
+after(function() {
+  // 블록 범위 내 모든 테스트 후에 실행
+  console.log('after');
+});
