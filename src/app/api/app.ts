e7304bfb6448be12b714eb95b8f5cadd49e6bda2
foway <foway0@gmail.com>
2020-02-17 02:50:20 +0900

    fix - move to debug middleware as first of middleware

--- src/app/api/app.ts
+++ src/app/api/app.ts
@@ -1,5 +1,6 @@
 const debug = process.env.DEBUG ? require('debug')('labofolio:app') : () => {};
 
+import * as express from 'express';
 import { OpenApiValidator } from 'express-openapi-validator';
 import * as path from 'path';
 
@@ -12,17 +13,20 @@ class ApiApplication extends Application {
   }
 
   async init(): Promise<void> {
-    await new OpenApiValidator({
-      apiSpec: path.join(__dirname, '../../api_specs/api.yaml'),
-      validateResponses: true,
-      operationHandlers: path.join(__dirname)
-    }).install(this.app);
-
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
     // middleware
     this.app.use((req, res, next) => {
       debug(`${this.host}:${this.port}${req.url}`);
       next();
     });
+
+    await new OpenApiValidator({
+      apiSpec: path.join(__dirname, '../../api_specs/api.yaml'),
+      validateRequests: true,
+      validateResponses: true,
+      operationHandlers: path.join(__dirname)
+    }).install(this.app);
 
     // error handler
     this.app.use(errorHandler);
